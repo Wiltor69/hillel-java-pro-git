e4713b19f206d2f476bc5d7f075894f7328ba821
Viacheslav <19wiltor69@gmail.com>
2022-10-26 16:56:50 +0300

    Add second commit

--- src/hillel/HillelJavaGit.java
+++ src/hillel/HillelJavaGit.java
@@ -3,6 +3,7 @@ package hillel;
 public class HillelJavaGit {
     public static void main(String[] args) {
         System.out.println("We learn Git");
+        System.out.println("Add new commit");
 
     }
 }
